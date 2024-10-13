89b8f719c1cc1ee4a609253128bab33faf1ac938
Hemanth Aditya Penke <148633482+HemanthPenke@users.noreply.github.com>
2024-10-13 11:39:54 +0530

    Add files via upload

---
+++ JavaScript/Arrays/1-dimensional-array.js
@@ -0,0 +1,24 @@
+//---------------1 Dimensional Array---------------
+
+
+
+let arr = [1,32,54,"Hemanth",true,8.24]
+
+console.log( "the array elements are : " ,arr )
+
+console.log("the 3rd element of the array is : " , arr[3])
+
+
+console.log("the length of the array is : ",arr.length)
+
+
+
+/*
+
+----------------------output------------------------------
+
+the array elements are :  [ 1, 32, 54, 'Hemanth', true, 8.24 ]
+the 3rd element of the array is :  Hemanth
+the length of the array is :  6
+
+*/
---
+++ JavaScript/Arrays/2-dimensional-array.js
@@ -0,0 +1,32 @@
+//---------------2 Dimensional Array---------------
+
+
+
+let arr = [
+    [12,34,43,23,34,23],
+    [43,342,234,34,75,23,87],
+    [75,24,86,13,5,12,86,13],
+    [344,23,56,23,98,13,676]
+
+]
+
+console.log( "the array elements are : " ,arr )
+
+console.log("the 3rd element of the array is : " , arr[3][4])
+
+console.log("the length of the array is : ",arr.length)
+
+console.log("the length of the sub array in the array is : ",arr[2].length)
+
+
+
+/*
+
+----------------------output------------------------------
+
+the array elements are :  [ [ 12, 34, 43, 23, 34, 23 ], [ 43, 342, 234, 34, 75,  23,  87 ], [ 75, 24, 86, 13, 5, 12, 86, 13 ], [ 344, 23,  56, 23, 98, 13, 676 ] ]
+the 3rd element of the array is :  98
+the length of the array is :  4
+the length of the sub array in the array is : 8
+
+*/
